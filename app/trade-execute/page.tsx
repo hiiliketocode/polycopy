aa3de3b10179daae75c1aa8f3f8969bc36e0aa7e
Rawdon Messenger <donraw@gmail.com>
2025-12-21 13:19:15 -0800

    force dynamic trade execution

--- app/trade-execute/page.tsx
+++ app/trade-execute/page.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+export const dynamic = 'force-dynamic'
+
 import { useCallback, useEffect, useMemo, useRef, useState } from 'react'
 import { useSearchParams } from 'next/navigation'
 import Header from '@/app/components/Header'
