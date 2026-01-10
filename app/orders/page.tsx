564763fa04175cde48f440a40784b1e9903b3213
Rawdon Messenger <donraw@gmail.com>
2026-01-09 16:52:31 -0800

    Remove re-exports from page file - Next.js pages can only have default export

--- app/orders/page.tsx
+++ app/orders/page.tsx
@@ -1,9 +1,5 @@
 import { OrdersScreen, type OrdersScreenProps } from '@/components/orders/OrdersScreen'
 
-// Re-export for backward compatibility
-export { OrdersScreen }
-export type { OrdersScreenProps }
-
 export type OrdersPageProps = OrdersScreenProps
 
 export default function OrdersPage(props: OrdersPageProps = {}) {
